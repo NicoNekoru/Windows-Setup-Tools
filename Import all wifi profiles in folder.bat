87267fc4a78e5c3dce5c6456937a2902b675c1d6
Neko <66227158+NicoNekoru@users.noreply.github.com>
2020-06-30 20:34:31 -0600

    Update Import all wifi profiles in folder.bat

--- Import all wifi profiles in folder.bat
+++ Import all wifi profiles in folder.bat
@@ -1,3 +1,3 @@
 echo off
-for %%a in ("C:\Users\kaima\OneDrive\Documents\Wifi Profiles\*.xml") do (netsh wlan add profile filename="%%a")
+for %%a in ("Wifi Profiles\*.xml") do (netsh wlan add profile filename="%%a")
 pause
